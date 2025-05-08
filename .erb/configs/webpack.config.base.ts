a122ca4d2a811b25e18551ac5ebef84b59f2b1a0
spoonbobo <seasonluke@gmail.com>
2025-05-08 10:29:38 +0800

    chore: update mcp deps

--- .erb/configs/webpack.config.base.ts
+++ .erb/configs/webpack.config.base.ts
@@ -9,50 +9,51 @@ import { dependencies as externals } from '../../release/app/package.json';
 import path from 'path';
 
 const configuration: webpack.Configuration = {
-  externals: [...Object.keys(externals || {})],
+    externals: [...Object.keys(externals || {})],
 
-  stats: 'errors-only',
+    stats: 'errors-only',
 
-  module: {
-    rules: [
-      {
-        test: /\.[jt]sx?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            // Remove this line to enable type checking in webpack builds
-            transpileOnly: true,
-            compilerOptions: {
-              module: 'nodenext',
-              moduleResolution: 'nodenext',
+    module: {
+        rules: [
+            {
+                test: /\.[jt]sx?$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'ts-loader',
+                    options: {
+                        // Remove this line to enable type checking in webpack builds
+                        transpileOnly: true,
+                        compilerOptions: {
+                            module: 'nodenext',
+                            moduleResolution: 'nodenext',
+                        },
+                    },
+                },
             },
-          },
-        },
-      },
-    ],
-  },
-
-  output: {
-    path: webpackPaths.srcPath,
-    // https://github.com/webpack/webpack/issues/1114
-    library: { type: 'commonjs2' },
-  },
-
-  /**
-   * Determine the array of extensions that should be used to resolve modules.
-   */
-  resolve: {
-    extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
-    modules: [webpackPaths.srcPath, 'node_modules'],
-    // There is no need to add aliases here, the paths in tsconfig get mirrored
-    plugins: [new TsconfigPathsPlugins()],
-    alias: {
-      '@': path.resolve(webpackPaths.rootPath, 'src'),
+        ],
     },
-  },
 
-  plugins: [new webpack.EnvironmentPlugin({ NODE_ENV: 'production' })],
+    output: {
+        path: webpackPaths.srcPath,
+        // https://github.com/webpack/webpack/issues/1114
+        library: { type: 'commonjs2' },
+    },
+
+    /**
+     * Determine the array of extensions that should be used to resolve modules.
+     */
+    resolve: {
+        extensions: ['.js', '.jsx', '.json', '.ts', '.tsx'],
+        modules: [webpackPaths.srcPath, 'node_modules'],
+        // There is no need to add aliases here, the paths in tsconfig get mirrored
+        plugins: [new TsconfigPathsPlugins()],
+        alias: {
+            '@': path.resolve(webpackPaths.rootPath, 'src'),
+            '@modelcontextprotocol/sdk': path.resolve(webpackPaths.rootPath, 'node_modules/@modelcontextprotocol/sdk/dist/esm'),
+        },
+    },
+
+    plugins: [new webpack.EnvironmentPlugin({ NODE_ENV: 'production' })],
 };
 
 export default configuration;
